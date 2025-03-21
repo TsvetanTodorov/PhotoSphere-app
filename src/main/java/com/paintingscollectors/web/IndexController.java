3f0f6915cf123deea9dfe7a20d5e909e2031de0e
Tsvetan <tstodorofff@gmail.com>
2025-03-21 11:22:06 +0200

    Injecting PaintingService in IndexController

--- src/main/java/com/paintingscollectors/web/IndexController.java
+++ src/main/java/com/paintingscollectors/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.paintingscollectors.web;
 
+import com.paintingscollectors.painting.model.Painting;
+import com.paintingscollectors.painting.service.PaintingService;
 import com.paintingscollectors.user.model.User;
 import com.paintingscollectors.user.service.UserService;
 import com.paintingscollectors.web.dto.LoginRequest;
@@ -14,16 +16,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.UUID;
 
 @Controller
 public class IndexController {
 
     private final UserService userService;
+    private final PaintingService paintingService;
 
     @Autowired
-    public IndexController(UserService userService) {
+    public IndexController(UserService userService, PaintingService paintingService) {
         this.userService = userService;
+        this.paintingService = paintingService;
     }
 
     @GetMapping("/")
@@ -84,9 +89,12 @@ public class IndexController {
         UUID userId = (UUID) session.getAttribute("user_id");
         User user =  userService.getById(userId);
 
+        List<Painting> allSystemPaintings =  paintingService.getAllPaintings();
+
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("home");
         modelAndView.addObject("user", user);
+        modelAndView.addObject("allSystemPaintings", allSystemPaintings);
 
         return modelAndView;
     }
